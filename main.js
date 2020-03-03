8f916c864e81051ea4db49d60d6de84048395422
Manuel J. Tovar <manuel.tovar@uao.edu.co>
2020-03-03 01:39:13 -0500

    Agrega logica para mover cards home in progress

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+function MoveRight(){
+
+    var div = document.getElementById("card-container");
+    div.setAttribute('style','transform: translate(10rem);');
+
+}
+
+function MoveLeft(){
+    
+    var div = document.getElementById("card-container");
+    div.setAttribute('style','transform: translate(-10rem);');
+
+}
