1b072549b7c31194de8ab346a9b1e41813502b8b
Manuel J. Tovar <manuel.tovar@uao.edu.co>
2020-03-04 17:41:03 -0500

    Se documenta el javascript

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
+/*Primitive function of buttons to move the cards to the right and
+left of food offered by the different restaurants affiliated with
+the page*/
 
+/*The functions consist of acquiring a div element and adding to it
+ a style that moves the container of the restaurant menus to the right
+and left*/
 
-function MoveRight(){
+function MoveRight() {
 
     var div = document.getElementById("card-container");
-    div.setAttribute('style','transform: translate(10rem);');
+    div.setAttribute('style', 'transform: translate(10rem);');
 
 }
 
-function MoveLeft(){
-    
+function MoveLeft() {
+
     var div = document.getElementById("card-container");
-    div.setAttribute('style','transform: translate(-10rem);');
+    div.setAttribute('style', 'transform: translate(-10rem);');
 
 }
